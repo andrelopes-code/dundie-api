c7c0d70a2a877075fe46706d8ee16b5a636be25f
André Lopes <andrelopes.code@gmail.com>
2024-03-30 16:36:09 -1200

    Adicionado um 'main_router' para centralizar todos os routers

--- dundie/app.py
+++ dundie/app.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 
-from dundie.routes.user import router as user_router
+from dundie.routes import main_router
 
 app = FastAPI(
     title='dundie', version='0.1.0', description='Dundie is a rewards API.'
 )
 
-# Routers
-app.include_router(router=user_router, prefix='/user', tags=['User'])
+app.include_router(main_router)
---
+++ dundie/routes/__init__.py
@@ -0,0 +1,6 @@
+from fastapi import APIRouter
+from dundie.routes.user import router as user_router
+
+main_router = APIRouter()
+
+main_router.include_router(router=user_router, prefix='/user', tags=['User'])
